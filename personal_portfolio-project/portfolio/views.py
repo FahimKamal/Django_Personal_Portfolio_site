d99d574e391bae6ee343d1d010a89ace713da9a4
FahimKamal <fahimkamal63@gmail.com>
2020-06-06 20:13:18 +0600

    4th commit The database is now connected with the template, and I can show the information from there.

--- personal_portfolio-project/portfolio/views.py
+++ personal_portfolio-project/portfolio/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Project
 
 
 # Create your views here.
-class HomePage(TemplateView):
+class HomePage(ListView):
+    model = Project
+    context_object_name = 'projects'
     template_name = 'portfolio/home.html'
