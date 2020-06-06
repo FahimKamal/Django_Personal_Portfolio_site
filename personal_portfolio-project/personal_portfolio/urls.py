1a949dd8a04976c9adc767fd52e0d87e73f2220f
FahimKamal <fahimkamal63@gmail.com>
2020-06-06 15:25:27 +0600

    4th commit home page is added.

--- personal_portfolio-project/personal_portfolio/urls.py
+++ personal_portfolio-project/personal_portfolio/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # This 2 imports is added so I can see uploaded images from my adminpage.
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePage.as_view(), name='homepage'),
+    path('portfolio/', include('portfolio.urls', namespace='portfolio')),
 ]
 
 # This line is added so I can see uploaded images from my adminpage.
