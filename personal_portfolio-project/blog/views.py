41b4e9ee96e0e07d4a76c6841cccac71fe79ce69
FahimKamal <fahimkamal63@gmail.com>
2020-06-07 20:08:27 +0600

    6th commit Now I have learned to connect multiple models with a single ListView class. Look in: blog/views.py

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Blog
+from portfolio.models import Project
 
 
 # Create your views here.
@@ -8,3 +9,15 @@ class BlogView(ListView):
     template_name = 'blog/all_blogs.html'
     model = Blog
     context_object_name = 'blogs'
+    # The code bellow will grap latest 2 post from all other posts in batabase.
+    queryset = Blog.objects.order_by('-date')[:2]
+
+    def get_queryset(self):
+        return Blog.objects.order_by('-date')[:2]
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """With This I can get objects from multiple models"""
+        context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.all()
+        return context
+
